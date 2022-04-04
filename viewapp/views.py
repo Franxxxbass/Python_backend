61b3d8389d45ebb56f89e1c427ebefd6b40cb23c
Franxxxbass <63103120+Franxxxbass@users.noreply.github.com>
2022-04-04 19:15:37 +0200

    made model Person

--- viewapp/views.py
+++ viewapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import HttpResponse, render
 from django import views
 
+from django.views.generic import TemplateView
+
 
 # function-based view
 def hello(request):
@@ -12,6 +14,7 @@ class HelloView(views.View):
     def get(self, request):
         return HttpResponse("eloł łorlt!")
 
+
 # functional-based view
 def hello_template(request):
     return render(
@@ -19,11 +22,52 @@ def hello_template(request):
         'viewapp/hello.html'
     )
 
+
 # class-based view
-class HelloClassicView(views.View):
+class HelloClassiView(views.View):
     def get(self, request):
         return render(
             request,
             'viewapp/hello.html'
         )
 
+
+# generic - view
+class HelloTemplateView(TemplateView):
+    template_name = 'viewapp/hello.html'
+
+
+# function - based view
+def hello_template2(request):
+    name = "Jan"
+    return render(
+        request,
+        'viewapp/hello2.html',
+        context={
+            "name": name,
+        }
+
+    )
+
+
+class HelloClassView2(views.View):
+    def get(self, request):
+        name = "Jan"
+        return render(
+            request,
+            'viewapp/hello2.html',
+            context={
+                "name": name,
+            }
+
+        )
+
+
+# generic view
+class HelloTemplateView2(TemplateView):
+    template_name = 'viewapp/hello2.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["name"] = "Jan"
+        return context
